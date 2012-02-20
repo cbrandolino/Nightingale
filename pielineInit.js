3c763af41333623799777c054e3456d7cd5c39e7
claudio brandolino <cbrandolinocbrandolino@gmail.com>
2012-02-20 18:15:07 +0100

    Plotter complete

--- pielineInit.js
+++ pielineInit.js
@@ -1,11 +1,9 @@
 var data = [
 	{name: 'red', value: 10},
 	{name: 'blue', value: 30},
-	{name: 'green', value:18},
-	{name: 'red', value: 10},
+	{name: 'blue', value:40},
 	{name: 'blue', value: 30},
-	{name: 'green', value:18},
-	{name: 'blue', value:40}
+	{name: 'blue', value:140}
 ];
 
 $(function() {
